5c3e82c2726f77c724cde8cc3fac1ad4a13671b6
PeraSite <pretocki3@gmail.com>
2025-09-30 20:22:55 +0900

    feat: vitest 추가

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+import "dotenv/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    env: process.env,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
